b72d14a45fe3d8817499a3a08f6b610426c83633
Sergio <steranz@gmail.com>
2022-12-27 20:35:30 -0300

    Agrega funcionalidad

--- src/Servidor/MarcoServidor.java
+++ src/Servidor/MarcoServidor.java
@@ -4,8 +4,10 @@ import java.awt.BorderLayout;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -35,37 +37,75 @@ public class MarcoServidor extends JFrame implements Runnable{
 	public void run() {
 		
 		try {
+			
 			ServerSocket servidor = new ServerSocket(9999);
 			String nick, ip, texto;
 			EnvioPaqueteDatos paqueteRecibido;
+			// Arreglo que almacena clientes conectados
+			ArrayList<String> listaIpConectados = new ArrayList<>();
 			
 			while(true) {
 			
-			Socket socket = servidor.accept();
-			ObjectInputStream flujoEntrada = new ObjectInputStream(socket.getInputStream());
-			paqueteRecibido = (EnvioPaqueteDatos) flujoEntrada.readObject();
-			
-			nick = paqueteRecibido.getNick();
-			ip = paqueteRecibido.getIp();
-			texto = paqueteRecibido.getTexto();
-			
-			areaTexto.append("nick: " + nick + "\n" +
-								"ip: " + ip + "\n" +
-								"mensaje: " + texto + "\n");	
-			
-			// Crear socket para salida de información
-			
-			Socket socketSalida = new Socket(ip, 9990);
-			
-			ObjectOutputStream paqueteSalida = new ObjectOutputStream(socketSalida.getOutputStream());
-			
-			paqueteSalida.writeObject(paqueteRecibido);
-			
-			socketSalida.close();
+				Socket socket = servidor.accept();
+				
+				ObjectInputStream flujoEntrada = new ObjectInputStream(socket.getInputStream());
+				
+				paqueteRecibido = (EnvioPaqueteDatos) flujoEntrada.readObject();
+				
+				nick = paqueteRecibido.getNick();
+				ip = paqueteRecibido.getIp();
+				texto = paqueteRecibido.getTexto();
+				
+				if (!texto.equals(" online")) {
+					
+					areaTexto.append("nick: " + nick + "\n" + "ip: " + ip + "\n" + "mensaje: " + texto + "\n");
+
+					// Crear socket para salida de información
+
+					Socket socketSalida = new Socket(ip, 9990);
+
+					ObjectOutputStream paqueteSalida = new ObjectOutputStream(socketSalida.getOutputStream());
+					
+					paqueteSalida.writeObject(paqueteRecibido);
+
+					socketSalida.close();
+					
+				}else {
+					
+					// Detectar usuarios que se conectan
+					
+					InetAddress direccionCliente = socket.getInetAddress();
+					
+					String ipClienteConectado = direccionCliente.getHostAddress();
+					
+					listaIpConectados.add(ipClienteConectado);
+					
+					System.out.println("Direccion remota coenctada: " + ipClienteConectado);
+					
+					// Agregar el arreglo al objeto a enviar
+					
+					paqueteRecibido.setDireccionesIp(listaIpConectados);
+					
+					for (String string : listaIpConectados) {
+						
+						System.out.println("ArrayList: " + string);
+						
+						// Enviar el arreglo a los clientes conectados
+						
+						Socket socketSalida = new Socket(string, 9990);
+
+						ObjectOutputStream paqueteSalida = new ObjectOutputStream(socketSalida.getOutputStream());
+						
+						paqueteSalida.writeObject(paqueteRecibido);
+
+						socketSalida.close();						
+						/////////////////////////////////////////////
+					}
+					
+					/////////////////////////////////////////////////////////////////////////
+				}
+				
 			}
-			
-			
-			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
